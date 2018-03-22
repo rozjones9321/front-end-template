21432dff2335beb8dab495fbac4580b3ce9ce9ae
Roz <rozjones1962@gmail.com>
2018-03-22 19:41:23 -0400

    images, pdfs and css changes

---
+++ js/java script lesson.js
@@ -0,0 +1,5 @@
+var counter = 1;
+while ( ) {
+    document.write("<p>Now in loop #" + counter + "</p>");
+    counter += 1;
+}
---
+++ js/object.js
@@ -0,0 +1,9 @@
+var newYork = {
+    population: 100, 
+    latitude: '40.7127 N',
+    longitude: '74.0059 W'
+  };
+  
+  newYork.population = 8.406e6;
+  newYork.country = 'USA';
+  
